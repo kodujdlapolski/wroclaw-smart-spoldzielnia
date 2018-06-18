b9b0a1e40abb4626332f60d282bad5ac79091e1c
Maciek <MagicFleas@bitbucket.org>
2018-06-18 21:30:08 +0200

    typos

--- src/api/RunAllTests.ps1
+++ src/api/RunAllTests.ps1
@@ -1,14 +1,20 @@
 Write-Host "Running Domain Tests" -Foreground DarkYellow
 dotnet run --project ./DomainTests/DomainTests.fsproj
+
 Write-Host "Running Infrastructure Tests" -Foreground DarkYellow
 dotnet run --project ./InfrastructureTests/InfrastructureTests.fsproj
+
 Write-Host "Running Application Tests" -Foreground DarkYellow
 dotnet run --project ./ApiTests/ApiTests.fsproj
-Write-Host "Running Starting Api" -Foreground DarkYellow
-Write-Host "Running Acceptance Tests" -Foreground DarkYellow
+
+Write-Host "Starting Api" -Foreground DarkYellow
 $job = Start-Job -ScriptBlock {dotnet run --project $using:PWD/Api/Api.fsproj}
 Start-Sleep -s 3 #yeah, I know :)
+
+Write-Host "Running Acceptance Tests" -Foreground DarkYellow
 dotnet run --project ./AcceptanceTests/AcceptanceTests.fsproj
-Write-Host "Running Stopping Api" -Foreground DarkYellow
+
+Write-Host "Stopping Api" -Foreground DarkYellow
 stop-job $job
-Write-Host "Running Done" -Foreground DarkYellow
+
+Write-Host "Done" -Foreground DarkYellow
